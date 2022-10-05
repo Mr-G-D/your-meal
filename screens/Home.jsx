e782831231e6c9c457c76c7d6de7ccef956fdf2b
Mr-G-D <daredinesh00@gmail.com>
2022-10-05 16:30:11 +0530

    api :thread:

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -5,14 +5,42 @@ import {
   View,
   ScrollView,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
 import RestaurantItem from "../components/RestaurantItem";
 import { localRestaurants } from "../components/RestaurantItem";
+import axios from "axios";
 
+const encodedParams = new URLSearchParams();
+encodedParams.append("language", "en_US");
+encodedParams.append("limit", "30");
+encodedParams.append("location_id", "297704");
+encodedParams.append("currency", "USD");
+console.log(encodedParams);
 const Home = () => {
+  const options = {
+    method: "POST",
+    url: "https://worldwide-restaurants.p.rapidapi.com/search",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "bd7fe14db0msh3feb308a98ca1dbp128d2ejsn6d48980f26d1",
+      "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com",
+    },
+    ...encodedParams,
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
   return (
     <SafeAreaView
       style={{
